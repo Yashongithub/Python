153e8e318b53ec4b189e030dad486bea43a09e48
Yash Tiwari <113037817+Yashongithub@users.noreply.github.com>
2022-11-24 14:00:40 +0530

    Python program to find compound interest

---
+++ program5.py
@@ -0,0 +1,10 @@
+p= 1200   # principle amount
+t= 2      # time
+r= 5.4    # rate
+# calculates the compound interest
+a=p*(1+(r/100))**t  # formula for calculating amount
+
+ci=a-p  # compound interest = amount - principal amount
+
+# printing compound interest value
+print(ci)
